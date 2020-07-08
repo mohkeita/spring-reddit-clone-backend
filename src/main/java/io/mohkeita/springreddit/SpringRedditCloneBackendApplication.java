2e9d9dc90b46009b2f45521e1fb7070ec53dcead
mohkeita <mohkeita13@gmail.com>
2020-07-08 20:03:11 +0200

    init project

---
+++ src/main/java/io/mohkeita/springreddit/SpringRedditCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.springreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mohkeita/springreddit/SpringRedditCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.springreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
