383ce80f29bb4e1e34be1ca8dfcdabb580dfca73
mohkeita <mohkeita13@gmail.com>
2020-07-09 02:45:45 +0200

    - Configure Spring Security - Implement Registration API

---
+++ src/main/java/io/mohkeita/springreddit/exceptions/SpringRedditException.java
@@ -0,0 +1,8 @@
+package io.mohkeita.springreddit.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
---
+++ src/main/java/io/mohkeita/springreddit/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package io.mohkeita.springreddit.repository;
+
+import io.mohkeita.springreddit.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+}
