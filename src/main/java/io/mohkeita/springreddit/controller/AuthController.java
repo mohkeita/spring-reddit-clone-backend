ee687348da393b1f5982f48a7a79883f3412fe72
mohkeita <mohkeita13@gmail.com>
2020-07-09 14:17:14 +0200

    Endpoint to verify users and Enable asynchronous processing

--- src/main/java/io/mohkeita/springreddit/controller/AuthController.java
+++ src/main/java/io/mohkeita/springreddit/controller/AuthController.java
@@ -3,12 +3,11 @@ package io.mohkeita.springreddit.controller;
 import io.mohkeita.springreddit.dto.RegisterRequest;
 import io.mohkeita.springreddit.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,6 +19,12 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
-        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
+        return new ResponseEntity<>("User Registration Successful", CREATED);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successfully", OK);
     }
 }
--- src/main/java/io/mohkeita/springreddit/service/AuthService.java
+++ src/main/java/io/mohkeita/springreddit/service/AuthService.java
@@ -1,6 +1,7 @@
 package io.mohkeita.springreddit.service;
 
 import io.mohkeita.springreddit.dto.RegisterRequest;
+import io.mohkeita.springreddit.exceptions.SpringRedditException;
 import io.mohkeita.springreddit.model.NotificationEmail;
 import io.mohkeita.springreddit.model.User;
 import io.mohkeita.springreddit.model.VerificationToken;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -49,5 +51,21 @@ public class AuthService {
 
         verificationTokenRepository.save(verificationToken);
         return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(() -> new SpringRedditException("Invalid Token"));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new SpringRedditException("User not found with name -" + username));
+
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
