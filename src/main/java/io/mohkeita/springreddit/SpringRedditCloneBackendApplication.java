744e0ea355c9dc97d417b86225c32c7f545e0b25
mohkeita <mohkeita13@gmail.com>
2020-07-12 23:55:07 +0200

    Document the REST API using Swagger and Springfox

--- src/main/java/io/mohkeita/springreddit/SpringRedditCloneBackendApplication.java
+++ src/main/java/io/mohkeita/springreddit/SpringRedditCloneBackendApplication.java
@@ -1,11 +1,14 @@
 package io.mohkeita.springreddit;
 
+import io.mohkeita.springreddit.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class SpringRedditCloneBackendApplication {
 
 	public static void main(String[] args) {
